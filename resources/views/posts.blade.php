67f56e171febaff3d0ac13ff5b27f6cb79843586
papic <danilo.p@vivifyideas.com>
2017-09-22 13:50:15 +0200

    Commit 4 - Refactored templates.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,18 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>All posts</title>
-    </head>
-    <body>
-        <h1>Posts</h1>
-        <ul>
-            @foreach($posts as $post)
-                <li><a href="{{ route('single-post', ['id' => $post->id]) }}">{{ $post->title }}</a></li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+@section('title')
+    All posts
+@endsection
+
+@section('content')
+    <h1>Posts</h1>
+    <ul>
+        @foreach($posts as $post)
+            <li><a href="{{ route('single-post', ['id' => $post->id]) }}">{{ $post->title }}</a></li>
+        @endforeach
+    </ul>
+@endsection
